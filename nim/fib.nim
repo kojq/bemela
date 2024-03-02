b9ba33fe94f2a72fbac5c64b1fa0ce90f6fb044a
kojq <106934605+kojq@users.noreply.github.com>
2024-03-01 20:07:35 -0500

    Delete nim/fib.nim

--- nim/fib.nim
+++
@@ -1,9 +0,0 @@
-import os, strutils
-
-proc fib(n: int): int =
-    if n <= 1: return n
-    else: return fib(n-1) + fib(n-2)
-
-when isMainModule:
-    let n = parseInt(paramStr(1))
-    echo fib(n)
